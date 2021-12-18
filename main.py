49fd1eb73fe0118f52f63067c2249994c5a6666b
sampaixd <samuelbroman03@gmail.com>
2021-12-18 13:28:16 +0100

    tested some new things

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import msvcrt
 import os
+import sys
 from colors import Color as c
 from content import Content
+from time import sleep
 #import pandas as pd
  
 def main():
@@ -11,7 +13,11 @@ def main():
     content.append(Content("vara 123", ["3info 1", "3info 2", "3info 3"]))
     content.append(Content("vara 1234", ["4info 1", "4info 2", "4info 3"]))
     content.append(Content("vara 12345", ["5info 1", "5info 2", "5info 3"]))
-    menu(content)
+    functions = [Purchase, HandleData, Exit]
+    title = "Welcome to mathem autobuyer 1.0!"
+    menucontent = ["Purchase wares", "Handle data", "Exit application"]
+
+    menuUI(title, menucontent, functions, content)
     print("Closing application...")
  
  
@@ -49,10 +55,10 @@ def menu(content):
                 else:
                     selectedcontent += 1
  
-            case "b'\\r'":
+            case "b'\\r'" | "b'd'":
                 contentInfo(content[selectedcontent])
 
-            case "b'q'":
+            case "b'q'" | "b'a'":
                 loop = False
         #print(keypressed)
             
@@ -69,11 +75,15 @@ def contentInfo(selectedcontent):
         if selectedinfo == 0:
             print(c.selected, end="")
 
-        print(selectedcontent.info[0] + c.default)
+        print("Add" + c.default)
+        print(" " * 30, end="")
+        if selectedinfo == 1:
+            print(c.selected, end="")
+        print("Back" + c.default)
 
-        for i in range(1, len(selectedcontent.info)):
+        for i in range(len(selectedcontent.info)):
             print(" " * 30, end="")
-            if selectedinfo == i:
+            if selectedinfo == i + 2:
                 print(c.selected, end="")
 
             print(selectedcontent.info[i] + c.default)
@@ -84,26 +94,136 @@ def contentInfo(selectedcontent):
  
             case "b'w'" | "b'H'":
                 if selectedinfo <= 0:
-                    selectedinfo = len(selectedcontent.info) - 1
+                    selectedinfo = len(selectedcontent.info) + 1
  
                 else:
                     selectedinfo -= 1
  
             case "b's'" | "b'P'":
  
-                if selectedinfo >= len(selectedcontent.info) - 1:
+                if selectedinfo >= len(selectedcontent.info) + 1:
                     selectedinfo = 0
                 else:
                     selectedinfo += 1
  
-            case "b'\\r'":
-                pass
+            case "b'\\r'" | "b'd'":
+                if selectedinfo == 0:
+                    addInfo(selectedcontent)
+                elif selectedinfo == 1:
+                    loop = False
+                else:
+                    changeInfo(selectedcontent, (selectedinfo - 2))
 
-            case "b'q'":
+            case "b'q'" | "b'a'":
                 loop = False
-    
- 
- 
+
+#called when changing name of info
+def addInfo(selectedcontent):
+    newinfo = input("Please enter the new info: ")
+    selectedcontent.info.append(newinfo)
+    print("New info added, returning to menu...")
+    sleep(1)
+
+#when changing name/deleting a specified info
+def changeInfo(selectedcontent, selectedinfo):  
+    loop = True
+    selectedchange = 0
+    while loop:
+        Clear()
+        try:
+            print(selectedcontent.name + " " * (30 - len(selectedcontent.name)) + 
+            selectedcontent.info[selectedinfo] + " " * (20 - len(selectedcontent.info[selectedinfo])), end="")
+            if selectedchange == 0:
+                print(c.selected, end="")
+            print("Back" + c.default)
+            print(" " * 50, end="")
+            if selectedchange == 1:
+                print(c.selected, end="")
+            print("Edit" + c.default)
+            print(" " * 50, end="")
+            if selectedchange == 2:
+                print(c.selected, end="")
+            print("Delete", c.default)
+            
+            print(c.black)
+            keypressed = str(msvcrt.getch())
+            match(keypressed):
+                case "b'w'" | "b'H'" :
+                    if selectedchange <= 0:
+                        selectedchange = 2
+                    else:
+                        selectedchange -= 1
+                case  "b's'" | "b'P'":
+                    if selectedchange >= 2:
+                        selectedchange = 0
+                    else:
+                        selectedchange += 1
+                case "b'q'" | "b'a'":
+                    loop = False
+
+                case "b'\\r'" | "b'd'":
+                    if selectedchange == 0:
+                        loop = False
+                    elif selectedchange == 1:
+                        newinfo = input("Please enter the new info: ")
+                        selectedcontent.info[selectedinfo] = newinfo
+                        print("Info updated!")
+                        sleep(1)
+                    else:
+                        selectedcontent.info.pop(selectedinfo)
+                        loop = False
+        except:
+            #print("No info in content, returning to prevoius menu...")
+            #sleep(1)
+            loop = False
+#test code, imports def into another def as a list
+###############################################################
+def Purchase(content):
+    print("purchasing products...")
+
+def HandleData(content):
+    menu(content)
+def Exit(content):
+    sys.exit()
+
+functions = [Purchase, HandleData, Exit]
+title = "Welcome to mathem autobuyer 1.0!"
+content = ["Purchase wares", "Handle data", "Exit application"]
+
+def menuUI(title, content, functions, data):
+    prevcontentspaces = 0
+    selected = 0
+    loop = True
+    while loop:
+        print(title)
+        print()
+        for i in range(len(content)):
+            if selected == i:
+                print(c.selected, end="")
+            print(content[i] + c.default)
+        print(c.black)
+        pressedkey = str(msvcrt.getch())
+        print(c.default)
+        Clear()
+        match(pressedkey):
+                case "b'w'" | "b'H'" :
+                    if selected <= 0:
+                        selected = len(content) - 1
+                    else:
+                        selected -= 1
+                case  "b's'" | "b'P'":
+                    if selected >= len(content) - 1:
+                        selected = 0
+                    else:
+                        selected += 1
+                case "b'q'" | "b'a'":
+                    loop = False
+
+                case "b'\\r'" | "b'd'":
+                    functions[selected](data)
+#####################################################################
+
+
        
 Clear = lambda: os.system('cls')
  
@@ -112,3 +232,4 @@ Clear = lambda: os.system('cls')
  
 if __name__ == '__main__':
     main()
+    #menuUI(title, content, functions)
